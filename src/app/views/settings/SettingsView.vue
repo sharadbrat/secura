d4b307392fd78ef677eb2897f6bdd03f94cc52c8
sharadbrat <george.sharadze@gmail.com>
2021-06-10 21:09:45 +0300

    finished settings view

--- src/app/views/settings/SettingsView.vue
+++ src/app/views/settings/SettingsView.vue
@@ -3,61 +3,56 @@
 
     <div class="settings-view">
 
-      <div>
-        <h2 class="settings-view__heading">Master key</h2>
-        <UiButton
-          class="settings-view__heading-button"
-          size="sm"
-          width="shrink"
-          shape="circled"
-          :type="isEditingMasterKey ? 'primary' : 'secondary'"
-          @click="isEditingMasterKey = !isEditingMasterKey"
-        >
-          <UiIcon name="edit"/>
-        </UiButton>
-      </div>
+      <section class="settings-view__section">
+        <h2 class="settings-view__heading">Set master key</h2>
+        <p class="settings-view__description">
+          The master key is used to generate passwords for all your saved services.
+          By default, it is only stored during a single session. However, you may
+          choose to store it in the local storage to share it between sessions,
+          but it is not recommended.
+        </p>
+        <p class="settings-view__description settings-view__description_highlighted">
+          It is not stored as-is (neither in a single session, nor in
+          local storage), that is why it is impossible to modify it, only to
+          set a new one.
+        </p>
+        <UiButton type="secondary" @click="onSetupMasterKeyClick">Set master key</UiButton>
+      </section>
 
-      <PasswordField
-        class="settings-view__password-field"
-        :readonly="isEditingMasterKey"
-        :value="masterKey"
-      />
+      <section class="settings-view__section">
+        <h2 class="settings-view__heading">Theme</h2>
+        <p>TBD</p>
+      </section>
 
-      <UiCheckbox
-        class="settings-view__master-key-checkbox"
-        v-model="isMasterKeyStored"
-        size="sm"
-      >
-        Store master key in local storage.
-      </UiCheckbox>
+      <section class="settings-view__section">
+        <h2 class="settings-view__heading">Danger zone</h2>
 
-      <p class="settings-view__master-key-description">
-        A master key is used to derive passwords for the services you want to
-        authenticate in. By default, it is not stored in the system and it is
-        deleted when you close Secura. If you want to, you can activate
-        storing the master key in local storage by clicking on the checkbox
-        above. In this case, please note that if an adversary gets access to
-        your device - they can easily use your master key to generate your
-        passwords.
-      </p>
+        <div class="settings-view__danger-zone">
+          <h3 class="settings-view__subheading">Delete services</h3>
+          <p class="settings-view__description">
+            Theme settings and the master key will remain, only saved services
+            will be deleted.
+          </p>
+          <UiButton size="sm" type="secondary" class="settings-view__delete">
+            <UiIcon class="settings-view__delete-icon" name="delete"/>
+            <span>Delete all saved services</span>
+          </UiButton>
 
-      <h2 class="settings-view__heading">Theme</h2>
+          <hr class="settings-view__danger-zone-delimiter"/>
 
-      <p>TBD</p>
+          <h3 class="settings-view__subheading">Delete everything</h3>
+          <p class="settings-view__description">
+            All the data will be deleted, you will be redirected to the main page.
+          </p>
 
-      <h2 class="settings-view__heading">Danger zone</h2>
+          <UiButton size="sm" class="settings-view__delete-button settings-view__delete-all">
+            <UiIcon class="settings-view__delete-icon" name="delete"/>
+            <span>Delete all data</span>
+          </UiButton>
+        </div>
+      </section>
 
-      <p>TBD</p>
-
-      <UiButton>
-        <UiIcon name="delete"/>
-        <span>Delete all saved services</span>
-      </UiButton>
-
-      <UiButton>
-        <UiIcon name="delete"/>
-        <span>Delete all data services</span>
-      </UiButton>
+      <SetupMasterKeyDialog ref="masterKeyDialog" @confirm="onSetupMasterKeyConfirm"/>
 
     </div>
   </SecondLevelLayout>
@@ -65,14 +60,16 @@
 
 <script lang="ts">
   import Vue from 'vue';
-  import { Component } from 'vue-property-decorator';
+  import { Component, Ref } from 'vue-property-decorator';
   import { State } from 'vuex-class';
+
+  import { LazyInject } from '@/core/ioc';
+  import { SetMasterKeyUseCase } from '@/core/use-case/keys/set-master-key.use-case';
 
   import SecondLevelLayout from '@/app/layouts/SecondLevelLayout.vue';
   import UiButton from '@/app/ui-kit/UiButton.vue';
   import UiIcon from '@/app/ui-kit/UiIcon.vue';
-  import UiCheckbox from '@/app/ui-kit/UiCheckbox.vue';
-  import PasswordField from '@/app/components/PasswordField.vue';
+  import SetupMasterKeyDialog from '@/app/components/SetupMasterKeyDialog.vue';
 
 
   @Component({
@@ -80,11 +77,13 @@
       SecondLevelLayout,
       UiButton,
       UiIcon,
-      UiCheckbox,
-      PasswordField,
+      SetupMasterKeyDialog,
     },
   })
   export default class SettingsView extends Vue {
+
+    @LazyInject(SetMasterKeyUseCase)
+    public setMasterKeyUseCase: SetMasterKeyUseCase;
 
     @State(state => state.keys.masterKey)
     public masterKey: string;
@@ -92,7 +91,19 @@
     @State(state => state.keys.isPersisted)
     public isMasterKeyStored: string;
 
+    @Ref()
+    public masterKeyDialog: SetupMasterKeyDialog;
+
     private isEditingMasterKey: boolean = false;
+
+    public onSetupMasterKeyClick() {
+      this.masterKeyDialog.show();
+    }
+
+    public onSetupMasterKeyConfirm(params: { key: string, isPersisted: boolean }) {
+      this.setMasterKeyUseCase.perform(params.key, params.isPersisted);
+      this.masterKeyDialog.hide();
+    }
 
   }
 </script>
@@ -108,21 +119,52 @@
       @include UiMargin(md, bottom);
     }
 
-    &__password-field {
+    &__subheading {
+      @include UiTypographyBody1();
+      @include UiMargin(xxs, bottom);
+    }
+
+    &__section {
+      @include UiMargin(md, bottom);
+      @include UiPadding(md, bottom);
+      border-bottom: 1px solid UiColor(shade-400);
+    }
+
+    &__description {
+      @include UiMargin(md, bottom);
+
+      &_highlighted {
+        @include UiBorderRadius(sm);
+        @include UiPadding(xs);
+        background-color: UiColor(neutral-light);
+        color: UiColor(neutral-dark);
+      }
+    }
+
+    &__delete {
+      border-color: UiColor(negative);
+      color: UiColor(negative);
+    }
+
+    &__delete-all {
+      background-color: UiColor(negative);
+    }
+
+    &__delete-icon {
       @include UiMargin(xxs, right);
-      flex-grow: 1;
+      margin-bottom: 2px;
     }
 
-    &__master-key-checkbox {
-      @include UiMargin(xs, top);
+    &__danger-zone {
+      @include UiBorderRadius(md);
+      @include UiPadding(sm);
+      border: 1px solid UiColor(negative-light);
     }
 
-    &__master-key-description {
-      @include UiPadding(xs);
-      @include UiMargin(sm, top);
-      @include UiBorderRadius(sm);
-      background-color: UiColor(neutral-light);
-      color: UiColor(neutral-dark);
+    &__danger-zone-delimiter {
+      @include UiMargin(md, top);
+      @include UiMargin(md, bottom);
+      border-bottom: 1px solid UiColor(shade-400);
     }
 
   }
