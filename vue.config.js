0490183921951b38fac5045a718be06c8c3ff920
sharadbrat <george.sharadze@gmail.com>
2021-06-15 11:01:26 +0300

    added cli-plugin-pwa

--- vue.config.js
+++ vue.config.js
@@ -16,5 +16,54 @@ module.exports = {
     disableHostCheck: true,
   },
   lintOnSave: 'warning',
-  filenameHashing: false,
+  pwa: {
+    name: 'Secura',
+    themeColor: '#29b6f6',
+    msTileColor: '#29b6f6',
+    manifestOptions: {
+      name: 'Secura',
+      short_name: 'Secura',
+      start_url: 'https://secura.netlify.app',
+      description: 'Secura: Headless password manager',
+      display: 'standalone',
+      orientation: 'portrait',
+      theme_color: '#29b6f6',
+      background_color: '#29b6f6',
+      icons: [
+        {
+          src: 'assets/icons/icon-192x192.png',
+          sizes: '192x192',
+          type: 'image/png',
+        },
+        {
+          src: 'assets/icons/icon-512x512.png',
+          sizes: '512x512',
+          type: 'image/png',
+        },
+        {
+          src: 'assets/icons/icon-192x192.png',
+          sizes: '192x192',
+          type: 'image/png',
+          purpose: 'maskable',
+        },
+        {
+          src: 'assets/icons/icon-512x512.png',
+          sizes: '512x512',
+          type: 'image/png',
+          purpose: 'maskable',
+        },
+      ],
+    },
+    iconPaths: {
+      favicon32: 'assets/icons/favicon-32x32.png',
+      favicon16: 'assets/icons/favicon-16x16.png',
+      appleTouchIcon: 'assets/icons/apple-touch-icon-152x152.png',
+      maskIcon: 'assets/icons/safari-pinned-tab.svg',
+      msTileImage: 'assets/icons/ms-tile-144x144.png',
+    },
+    appleMobileWebAppCapable: 'yes',
+    workboxOptions: {
+      navigateFallback: 'index.html',
+    },
+  },
 };
